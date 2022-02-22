e82c6bc7d31d1d03e06d660029bd64b51d125720
Edwin <edwin@inqbeta.hk>
2022-02-22 22:30:42 +0100

    add tests for tpl/internal/go_templates/fmtsort.compare()

---
+++ tpl/internal/go_templates/fmtsort/sort_internal_test.go
@@ -0,0 +1,72 @@
+package fmtsort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCompareDifferentType(t *testing.T) {
+	a := byte(0)
+	b := bool(false)
+
+	if v := compare(reflect.ValueOf(a), reflect.ValueOf(b)); v != -1 {
+		t.Fail()
+	}
+}
+
+func TestCompareNilChan(t *testing.T) {
+	var at chan interface{}
+	a := reflect.ValueOf(at)
+
+	var bt chan interface{}
+	b := reflect.ValueOf(bt)
+
+	if v := compare(a, b); v != 0 {
+		t.Fail()
+	}
+}
+
+func TestCompareFuncs(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			return
+		}
+
+		t.Fail()
+	}()
+
+	var at = func() {}
+	a := reflect.ValueOf(at)
+
+	var bt = func() {}
+	b := reflect.ValueOf(bt)
+
+	compare(a, b)
+}
+
+/*
+type nilInf struct {
+	reflect.Value
+}
+
+func (nilInf) IsNil() bool {
+	return true
+}
+
+func TestCompareNilInterface(t *testing.T) {
+	a := reflect.ValueOf(interface{}(1))
+	a.
+
+	b := nilInf{reflect.ValueOf(interface{}(1))}
+
+	t.Logf("%t", a.IsNil())
+	t.Logf("%t", b.IsNil())
+	t.Logf("%t", a.Type() == b.Type())
+	t.Logf("%v", a.Kind())
+
+	if v := compare(a, b); v != -1 {
+		t.Log(v)
+		t.Fail()
+	}
+}
+*/
