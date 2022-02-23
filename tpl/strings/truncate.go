13e2cd28c8bd28840320433fa88d4bf11c9a24a0
tobiasgg <tobiasgg@kth.se>
2022-02-23 12:52:25 +0100

    feat: Implements manual branch coverage of function Truncate in tpl/strings/truncate.go Fixes #7

--- tpl/strings/truncate.go
+++ tpl/strings/truncate.go
@@ -15,13 +15,12 @@ package strings
 
 import (
 	"errors"
+	"github.com/spf13/cast"
 	"html"
 	"html/template"
 	"regexp"
 	"unicode"
 	"unicode/utf8"
-
-	"github.com/spf13/cast"
 )
 
 var (
@@ -40,118 +39,219 @@ type htmlTag struct {
 }
 
 // Truncate truncates a given string to the specified length.
-func (ns *Namespace) Truncate(a interface{}, options ...interface{}) (template.HTML, error) {
+func (ns *Namespace) Truncate(a interface{}, coverageMap map[int][]bool, options ...interface{}) (template.HTML, error) {
 	length, err := cast.ToIntE(a)
+	coverageMap[0][0] = true
 	if err != nil {
+		coverageMap[0][1] = true
 		return "", err
 	}
+	coverageMap[0][2] = true
 	var textParam interface{}
 	var ellipsis string
-
+	coverageMap[1][0] = true
+	coverageMap[2][0] = true
+	coverageMap[3][0] = true
 	switch len(options) {
 	case 0:
+		coverageMap[1][1] = true
+		coverageMap[2][2] = true
+		coverageMap[3][2] = true
 		return "", errors.New("truncate requires a length and a string")
 	case 1:
+		coverageMap[2][1] = true
+		coverageMap[1][2] = true
+		coverageMap[3][2] = true
 		textParam = options[0]
 		ellipsis = " …"
 	case 2:
+		coverageMap[3][1] = true
+		coverageMap[1][2] = true
+		coverageMap[2][2] = true
 		textParam = options[1]
 		ellipsis, err = cast.ToStringE(options[0])
+		coverageMap[4][0] = true
 		if err != nil {
+			coverageMap[4][1] = true
 			return "", errors.New("ellipsis must be a string")
 		}
+		coverageMap[4][2] = true
+		coverageMap[5][0] = true
 		if _, ok := options[0].(template.HTML); !ok {
+			coverageMap[5][1] = true
 			ellipsis = html.EscapeString(ellipsis)
 		}
+		coverageMap[5][2] = true
 	default:
+		coverageMap[1][2] = true
+		coverageMap[2][2] = true
+		coverageMap[3][2] = true
 		return "", errors.New("too many arguments passed to truncate")
 	}
+	coverageMap[6][0] = true
 	if err != nil {
+		coverageMap[6][1] = true
 		return "", errors.New("text to truncate must be a string")
 	}
+	coverageMap[6][2] = true
 	text, err := cast.ToStringE(textParam)
+	coverageMap[7][0] = true
 	if err != nil {
+		coverageMap[7][1] = true
 		return "", errors.New("text must be a string")
 	}
+	coverageMap[7][2] = true
 
 	_, isHTML := textParam.(template.HTML)
-
+	coverageMap[8][0] = true
 	if utf8.RuneCountInString(text) <= length {
+		coverageMap[8][1] = true
+		coverageMap[9][0] = true
 		if isHTML {
+			coverageMap[9][1] = true
 			return template.HTML(text), nil
 		}
+		coverageMap[9][2] = true
 		return template.HTML(html.EscapeString(text)), nil
 	}
+	coverageMap[8][2] = true
 
 	tags := []htmlTag{}
 	var lastWordIndex, lastNonSpace, currentLen, endTextPos, nextTag int
-
+	coverageMap[10][0] = true
+	coverageMap[10][2] = true
 	for i, r := range text {
+		coverageMap[10][1] = true
+		coverageMap[11][0] = true
 		if i < nextTag {
+			coverageMap[11][1] = true
 			continue
 		}
-
+		coverageMap[11][2] = true
+		coverageMap[12][0] = true
 		if isHTML {
+			coverageMap[12][1] = true
 			// Make sure we keep tag of HTML tags
 			slice := text[i:]
 			m := tagRE.FindStringSubmatchIndex(slice)
+			coverageMap[13][0] = true
+			coverageMap[14][0] = true
 			if len(m) > 0 && m[0] == 0 {
+				coverageMap[13][1] = true
+				coverageMap[14][1] = true
 				nextTag = i + m[1]
 				tagname := slice[m[4]:m[5]]
 				lastWordIndex = lastNonSpace
 				_, singlet := htmlSinglets[tagname]
+				coverageMap[15][0] = true
+				coverageMap[16][0] = true
 				if !singlet && m[6] == -1 {
+					coverageMap[15][1] = true
+					coverageMap[16][1] = true
 					tags = append(tags, htmlTag{name: tagname, pos: i, openTag: m[2] == -1})
+				}
+				if singlet && m[6] == -1 {
+					coverageMap[15][2] = true
+				}
+				if !singlet && m[6] != -1 {
+					coverageMap[16][2] = true
 				}
 
 				continue
 			}
+			coverageMap[13][2] = true
+			coverageMap[14][2] = true
 		}
+		coverageMap[12][2] = true
 
 		currentLen++
+		coverageMap[17][0] = true
+		coverageMap[18][0] = true
 		if unicode.IsSpace(r) {
+			coverageMap[17][1] = true
+			coverageMap[18][2] = true
 			lastWordIndex = lastNonSpace
 		} else if unicode.In(r, unicode.Han, unicode.Hangul, unicode.Hiragana, unicode.Katakana) {
+			coverageMap[18][1] = true
+			coverageMap[17][2] = true
 			lastWordIndex = i
 		} else {
+			coverageMap[17][2] = true
+			coverageMap[18][2] = true
 			lastNonSpace = i + utf8.RuneLen(r)
 		}
-
+		coverageMap[19][0] = true
 		if currentLen > length {
+			coverageMap[19][1] = true
+			coverageMap[20][0] = true
 			if lastWordIndex == 0 {
+				coverageMap[20][1] = true
 				endTextPos = i
 			} else {
+				coverageMap[20][2] = true
 				endTextPos = lastWordIndex
 			}
 			out := text[0:endTextPos]
+			coverageMap[21][0] = true
 			if isHTML {
+				coverageMap[21][1] = true
 				out += ellipsis
 				// Close out any open HTML tags
 				var currentTag *htmlTag
+				coverageMap[22][0] = true
 				for i := len(tags) - 1; i >= 0; i-- {
 					tag := tags[i]
+					coverageMap[22][1] = true
+					coverageMap[23][0] = true
+					coverageMap[24][0] = true
 					if tag.pos >= endTextPos || currentTag != nil {
+						if tag.pos >= endTextPos && currentTag == nil {
+							coverageMap[23][1] = true
+						} else {
+							coverageMap[24][1] = true
+						}
+						coverageMap[25][0] = true
+						coverageMap[26][0] = true
 						if currentTag != nil && currentTag.name == tag.name {
+							coverageMap[25][1] = true
+							coverageMap[26][1] = true
 							currentTag = nil
+						} else if currentTag != nil && currentTag.name != tag.name {
+							coverageMap[25][1] = true
+							coverageMap[26][2] = true
+						} else if currentTag != nil {
+							coverageMap[25][2] = true
+						} else {
+							coverageMap[25][2] = true
+							coverageMap[26][2] = true
 						}
 						continue
 					}
+					coverageMap[23][2] = true
+					coverageMap[24][2] = true
 
+					coverageMap[27][0] = true
 					if tag.openTag {
+						coverageMap[27][1] = true
 						out += ("</" + tag.name + ">")
 					} else {
+						coverageMap[27][2] = true
 						currentTag = &tag
 					}
 				}
+				coverageMap[22][2] = true
 
 				return template.HTML(out), nil
 			}
 			return template.HTML(html.EscapeString(out) + ellipsis), nil
 		}
+		coverageMap[19][2] = true
 	}
-
+	coverageMap[28][0] = true
 	if isHTML {
+		coverageMap[28][1] = true
 		return template.HTML(text), nil
 	}
+	coverageMap[28][2] = true
 	return template.HTML(html.EscapeString(text)), nil
 }
--- tpl/strings/truncate_test.go
+++ tpl/strings/truncate_test.go
@@ -14,8 +14,10 @@
 package strings
 
 import (
+	"fmt"
 	"html/template"
 	"reflect"
+	"sort"
 	"strings"
 	"testing"
 )
@@ -24,40 +26,45 @@ func TestTruncate(t *testing.T) {
 	t.Parallel()
 
 	var err error
+	var coverageMap = make(map[int][]bool)
+	for i := 0; i < 29; i++ {
+		coverageMap[i] = []bool{false, false, false}
+	}
 	cases := []struct {
 		v1    interface{}
+		m     map[int][]bool
 		v2    interface{}
 		v3    interface{}
 		want  interface{}
 		isErr bool
 	}{
-		{10, "I am a test sentence", nil, template.HTML("I am a …"), false},
-		{10, "", "I am a test sentence", template.HTML("I am a"), false},
-		{10, "", "a b c d e f g h i j k", template.HTML("a b c d e"), false},
-		{12, "", "<b>Should be escaped</b>", template.HTML("&lt;b&gt;Should be"), false},
-		{10, template.HTML(" <a href='#'>Read more</a>"), "I am a test sentence", template.HTML("I am a <a href='#'>Read more</a>"), false},
-		{20, template.HTML("I have a <a href='/markdown'>Markdown link</a> inside."), nil, template.HTML("I have a <a href='/markdown'>Markdown …</a>"), false},
-		{10, "IamanextremelylongwordthatjustgoesonandonandonjusttoannoyyoualmostasifIwaswritteninGermanActuallyIbettheresagermanwordforthis", nil, template.HTML("Iamanextre …"), false},
-		{10, template.HTML("<p>IamanextremelylongwordthatjustgoesonandonandonjusttoannoyyoualmostasifIwaswritteninGermanActuallyIbettheresagermanwordforthis</p>"), nil, template.HTML("<p>Iamanextre …</p>"), false},
-		{13, template.HTML("With <a href=\"/markdown\">Markdown</a> inside."), nil, template.HTML("With <a href=\"/markdown\">Markdown …</a>"), false},
-		{14, "Hello中国 Good 好的", nil, template.HTML("Hello中国 Good 好 …"), false},
-		{15, "", template.HTML("A <br> tag that's not closed"), template.HTML("A <br> tag that's"), false},
-		{14, template.HTML("<p>Hello中国 Good 好的</p>"), nil, template.HTML("<p>Hello中国 Good 好 …</p>"), false},
-		{2, template.HTML("<p>P1</p><p>P2</p>"), nil, template.HTML("<p>P1 …</p>"), false},
-		{3, template.HTML(strings.Repeat("<p>P</p>", 20)), nil, template.HTML("<p>P</p><p>P</p><p>P …</p>"), false},
-		{18, template.HTML("<p>test <b>hello</b> test something</p>"), nil, template.HTML("<p>test <b>hello</b> test …</p>"), false},
-		{4, template.HTML("<p>a<b><i>b</b>c d e</p>"), nil, template.HTML("<p>a<b><i>b</b>c …</p>"), false},
-		{10, nil, nil, template.HTML(""), true},
-		{nil, nil, nil, template.HTML(""), true},
+		{10, coverageMap, "I am a test sentence", nil, template.HTML("I am a …"), false},
+		{10, coverageMap, "", "I am a test sentence", template.HTML("I am a"), false},
+		{10, coverageMap, "", "a b c d e f g h i j k", template.HTML("a b c d e"), false},
+		{12, coverageMap, "", "<b>Should be escaped</b>", template.HTML("&lt;b&gt;Should be"), false},
+		{10, coverageMap, template.HTML(" <a href='#'>Read more</a>"), "I am a test sentence", template.HTML("I am a <a href='#'>Read more</a>"), false},
+		{20, coverageMap, template.HTML("I have a <a href='/markdown'>Markdown link</a> inside."), nil, template.HTML("I have a <a href='/markdown'>Markdown …</a>"), false},
+		{10, coverageMap, "IamanextremelylongwordthatjustgoesonandonandonjusttoannoyyoualmostasifIwaswritteninGermanActuallyIbettheresagermanwordforthis", nil, template.HTML("Iamanextre …"), false},
+		{10, coverageMap, template.HTML("<p>IamanextremelylongwordthatjustgoesonandonandonjusttoannoyyoualmostasifIwaswritteninGermanActuallyIbettheresagermanwordforthis</p>"), nil, template.HTML("<p>Iamanextre …</p>"), false},
+		{13, coverageMap, template.HTML("With <a href=\"/markdown\">Markdown</a> inside."), nil, template.HTML("With <a href=\"/markdown\">Markdown …</a>"), false},
+		{14, coverageMap, "Hello中国 Good 好的", nil, template.HTML("Hello中国 Good 好 …"), false},
+		{15, coverageMap, "", template.HTML("A <br> tag that's not closed"), template.HTML("A <br> tag that's"), false},
+		{14, coverageMap, template.HTML("<p>Hello中国 Good 好的</p>"), nil, template.HTML("<p>Hello中国 Good 好 …</p>"), false},
+		{2, coverageMap, template.HTML("<p>P1</p><p>P2</p>"), nil, template.HTML("<p>P1 …</p>"), false},
+		{3, coverageMap, template.HTML(strings.Repeat("<p>P</p>", 20)), nil, template.HTML("<p>P</p><p>P</p><p>P …</p>"), false},
+		{18, coverageMap, template.HTML("<p>test <b>hello</b> test something</p>"), nil, template.HTML("<p>test <b>hello</b> test …</p>"), false},
+		{4, coverageMap, template.HTML("<p>a<b><i>b</b>c d e</p>"), nil, template.HTML("<p>a<b><i>b</b>c …</p>"), false},
+		{10, coverageMap, nil, nil, template.HTML(""), true},
+		{nil, coverageMap, nil, nil, template.HTML(""), true},
 	}
 	for i, c := range cases {
 		var result template.HTML
 		if c.v2 == nil {
-			result, err = ns.Truncate(c.v1)
+			result, err = ns.Truncate(c.v1, c.m)
 		} else if c.v3 == nil {
-			result, err = ns.Truncate(c.v1, c.v2)
+			result, err = ns.Truncate(c.v1, c.m, c.v2)
 		} else {
-			result, err = ns.Truncate(c.v1, c.v2, c.v3)
+			result, err = ns.Truncate(c.v1, c.m, c.v2, c.v3)
 		}
 
 		if c.isErr {
@@ -76,8 +83,42 @@ func TestTruncate(t *testing.T) {
 	}
 
 	// Too many arguments
-	_, err = ns.Truncate(10, " ...", "I am a test sentence", "wrong")
+	_, err = ns.Truncate(10, coverageMap, " ...", "I am a test sentence", "wrong")
 	if err == nil {
 		t.Errorf("Should have errored")
 	}
+	branchesCovered := 0
+	totalBranches := 2 * len(coverageMap)
+	keys := make([]int, 0, len(coverageMap))
+	for k := range coverageMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	fmt.Println("")
+	fmt.Println("----------------Branch Coverage Report----------------")
+	fmt.Println("")
+	for _, k := range keys {
+		element := coverageMap[k]
+		if !element[0] {
+			fmt.Println("Condition : ", k, " was never reached")
+		}
+		if element[1] {
+			branchesCovered += 1
+			if !element[2] {
+				fmt.Println("Condition : ", k, " evaluated only to true but never false")
+			}
+		}
+		if element[2] {
+			branchesCovered += 1
+			if !element[1] {
+				fmt.Println("Condition : ", k, " evaluated only to false but never true")
+			}
+		}
+
+	}
+	covPercentage := float32(branchesCovered) / float32(totalBranches) * 100
+	fmt.Println("")
+	fmt.Println("Branches covered: ", branchesCovered, "/", totalBranches, "=", covPercentage, "%")
+	fmt.Println("")
+	fmt.Println("-------------------------------------------------------")
 }
